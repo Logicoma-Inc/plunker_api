6c5044249250676d1cf5e3ab76ffa23f7ad6a2cd
Åℵ┬ℏÔñÝ ℉⅍§ë‡ <fassetar@users.noreply.github.com>
2017-09-20 02:26:07 -0400

    Update and rename package.coffee to package.js

---
+++ database/schema/package.js
@@ -0,0 +1,81 @@
+var Document, Mixed, ObjectId, PackageDependencySchema, PackageSchema, PackageVersionSchema, Query, Schema, mongoose, ref;
+
+mongoose = require("mongoose");
+
+Schema = mongoose.Schema, Document = mongoose.Document, Query = mongoose.Query;
+
+ref = Schema.Types, ObjectId = ref.ObjectId, Mixed = ref.Mixed;
+
+PackageDependencySchema = new Schema({
+  name: String,
+  range: String
+});
+
+PackageVersionSchema = new Schema({
+  semver: String,
+  unstable: {
+    type: Boolean,
+    "default": false
+  },
+  scripts: [String],
+  styles: [String],
+  dependencies: [PackageDependencySchema]
+});
+
+PackageSchema = new Schema({
+  name: {
+    type: String,
+    match: /^[-_.a-z0-9]+$/i,
+    index: true,
+    unique: true
+  },
+  created_at: {
+    type: Date,
+    "default": Date.now()
+  },
+  versionCount: {
+    type: Number,
+    "default": 0
+  },
+  description: {
+    type: String
+  },
+  homepage: String,
+  documentation: String,
+  keywords: [
+    {
+      type: String,
+      index: true
+    }
+  ],
+  versions: [PackageVersionSchema],
+  categories: [String],
+  bumps: {
+    type: Number,
+    "default": 0,
+    index: true
+  },
+  maintainers: [
+    {
+      type: String,
+      index: true
+    }
+  ]
+});
+
+
+/*
+PackageSchema.index {
+  name: "text"
+  description: "text"
+  keywords: "text"
+}, {
+  name: "typeahead"
+  weights:
+    name: 3
+    description: 1
+    keywords: 2
+}
+ */
+
+exports.PackageSchema = PackageSchema;
